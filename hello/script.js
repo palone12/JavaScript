2525d869e10bb2f0fea17c2251f04d527d3fa671
Pranay Alone <palone12>
2022-06-12 20:00:17 +0530

    JS Commit

---
+++ hello/script.js
@@ -0,0 +1,76 @@
+
+// Sign Up Form;
+var firstName ="Pranay"
+var lastName ="Alone"
+var email= "alonepranay123@gmail.com"
+var Dob =29031999
+var password ="lawdaLasun"
+
+
+console.log(`
+   First Name :${firstName}
+   Last Name : ${lastName}
+   Email: ${email}
+   Date of Birth : ${Dob}
+  
+
+
+`)
+// var myHeading = document.querySelector("h1");
+// myHeading.textContent="Hello Bhosdiwalo";
+// console.log(myHeading);
+/* Operations in 
+   javaScript */
+ 
+   //Adittion;
+   var a = 10 ;
+   var b =56;
+   console.log( "addition is " ,a+b);
+   console.log("Substraction", b-a);
+   console.log("Multiplicatuiom " ,a*b);
+   console.log("Division",a/b);
+
+   // Some Other  Operators
+   var sex= 69;
+   var noSex = 696;
+   console.log(sex===noSex);
+
+
+   // Coditionals
+
+   var temp =30;
+   if(temp<30){
+      console.log("It is little cold Outside");
+   }
+   else{
+      console.log("It  is very hot Outside");
+   }
+
+   // Make a Purchase
+   var email =false;
+   var cardInfo = true;
+   var Cvv = true;
+   if(email){
+      if(cardInfo){
+         if(Cvv){
+            console.log("Everythig is valid")
+         }
+      }
+   }
+   
+
+   // In One go
+ 
+ if(email&& cardInfo&&Cvv){
+    console.log("You can buy  the product");
+ }
+ else{
+    console.log("Tera Bapp ya chod gya tha kya 10 20 crore");
+ }
+ //OR Operattor
+ if(email||  cardInfo|| Cvv){
+   console.log("You can buy  the product");
+}
+else{
+   console.log("Tera Bapp ya chod gya tha kya 10 20 crore");
+}
