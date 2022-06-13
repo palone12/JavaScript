c208f5437f9436bdd3dc83e327905fc0fe073c71
Pranay Alone <palone12>
2022-06-13 22:19:02 +0530

    New Changes

--- hello/Class.js
+++ hello/Class.js
@@ -10,3 +10,36 @@ console.log(name1==name2)
 const arr = [1,2,3,4,5];
 const arr2= arr;
 console.log(arr===arr2)
+
+
+// Arrays
+//Declaretion of Arr 
+//1st t way
+let array = [0,1,56,56];
+console.log(array)
+
+
+//Adding element at the end of the of the array
+array.push(5666)
+console.log(array)
+
+// Remove the element of the array
+array.pop();
+array.shift();
+
+console.log(array)
+
+
+// Deleting Element in Array 
+delete array[2];
+console.log(array)
+
+array.push(10);
+array.push(11);
+array.push(13);
+array.push(14);
+// Slice and Splice
+console.log(array)
+array .splice(2,3)
+console.log(array)
+console.log(array.splice(-1));
